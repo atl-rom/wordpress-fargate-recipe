0e7ed555b56ae3e66d0e10885b284ca62955e491
atlrom <romgomez88@yahoo.co.uk>
2021-04-03 14:43:24 +0100

    fix variables

--- variables.tf
+++ variables.tf
@@ -10,16 +10,20 @@ variable "server_region" {
 variable "app_name" {
   type        = string
   description = "Application name"
+  default = "aeronautics"
 }
 variable "app_environment" {
   type        = string
   description = "Application environment"
+  default = "wp-test"
 }
 variable "admin_sources_cidr" {
   type        = list(string)
   description = "List of IPv4 CIDR blocks from which to allow admin access"
+  default = ["0.0.0.0/0"]
 }
 variable "app_sources_cidr" {
   type        = list(string)
   description = "List of IPv4 CIDR blocks from which to allow application access"
+  default = ["0.0.0.0/0"]
 }
